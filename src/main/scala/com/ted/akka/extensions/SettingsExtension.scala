7ae4b749db7293b79910d74389b0f0620b8f86d4
Tudor Zgureanu <tzgureanu@gmail.com>
2016-09-13 23:08:43 +0100

    Add Settings Extension.

--- src/main/scala/com/ted/akka/extensions/SettingsExtension.scala
+++ src/main/scala/com/ted/akka/extensions/SettingsExtension.scala
@@ -5,6 +5,24 @@ import com.typesafe.config.Config
 
 
 class SettingsExtensionImpl(config: Config) extends Extension {
+
+  object service {
+    private val serviceConfig = config.getConfig("service")
+    val name = serviceConfig.getString("name")
+  }
+
+  object productService {
+    private val productServiceConfig = config.getConfig("product-service")
+    val protocol = productServiceConfig.getString("protocol")
+    val host = productServiceConfig.getString("host")
+    val port = productServiceConfig.getString("port")
+
+    object products {
+      private val productsConfig = productServiceConfig.getConfig("products")
+      val uri = productsConfig.getString("uri")
+    }
+  }
+
 }
 
 object SettingsExtension extends ExtensionId[SettingsExtensionImpl] with ExtensionIdProvider {
